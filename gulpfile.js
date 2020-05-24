7b2d9e148bf04473445fd312e0a28700540c4b49
William Ward <william.ward074@gmail.com>
2020-05-24 20:37:42 +0100

    Listen to all script files using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function(){
   return gulp.src([ // The order that you list the files in this array IS IMPORTANT!!
-      'assets/scripts/main.js',
+      'assets/scripts/**.*',
     ])
   .pipe(concat('scripts.min.js')) // concatenates the JS files listed above into one file called scripts.min.js
   .pipe(uglify({
