b9291f06a089fedf5f29bd4d9b0d31041aa86e1d
William Ward <william.ward074@gmail.com>
2020-06-04 18:56:33 +0100

    Add show in menu to page fixtures

--- src/DataFixtures/PageFixtures.php
+++ src/DataFixtures/PageFixtures.php
@@ -16,6 +16,7 @@ class PageFixtures extends Fixture
 	        $page->setRoute('home');
 	        $page->setSlug('/');
 	        $page->setShowInSitemap(true);
+	        $page->setShowInMenu(false);
 	        $page->setTitle('Home');
 	        $page->setMetaTitle('Welcome to my website');
 	        $page->setMetaDescription('This is my new website');
@@ -26,6 +27,7 @@ class PageFixtures extends Fixture
 	        $page->setRoute('contact_us');
 	        $page->setSlug('contact-us');
 	        $page->setShowInSitemap(true);
+	        $page->setShowInMenu(true);
 	        $page->setTitle('Contact Us');
 	        $page->setMetaTitle('Contact us today');
 	        $page->setMetaDescription('Have an enquiry or need to get in contact with us?');
@@ -36,6 +38,7 @@ class PageFixtures extends Fixture
 	        $page->setRoute('contact_us_success');
 	        $page->setSlug('contact-us/success');
 	        $page->setShowInSitemap(false);
+	        $page->setShowInMenu(false);
 	        $page->setTitle('Contact form received');
 	        $page->setMetaTitle('Thank you! Contact form received');
 	        $page->setMetaDescription('Thank you, we have recievied your submission');
@@ -46,6 +49,7 @@ class PageFixtures extends Fixture
 	        $page->setRoute('about_us');
 	        $page->setSlug('about-us');
 	        $page->setShowInSitemap(true);
+	        $page->setShowInMenu(true);
 	        $page->setTitle('About us');
 	        $page->setMetaTitle('About us');
 	        $page->setMetaDescription('Learn more about us!');
